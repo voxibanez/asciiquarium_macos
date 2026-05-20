b8e19add1ac354e29d50fad8b12cb60502c4cf70
Tim Barber <voxibanez@users.noreply.github.com>
2026-05-19 20:25:58 -0400

    Update ascii-fishtank cask version to 1.1.0
    Signed-off-by: Tim Barber <voxibanez@users.noreply.github.com>

--- Casks/ascii-fishtank.rb
+++ Casks/ascii-fishtank.rb
@@ -1,6 +1,6 @@
 cask "ascii-fishtank" do
-  version "1.0.9"
-  sha256 "80f0d2e2acb56e292b7762686d114e68bb95b5df6f07c92c047ad3500b71cfd9"
+  version "1.1.0"
+  sha256 "51e3671561f17255f2dd9e9abe30cd2fb8e8951838090a8694dd48fba612a3f0"
 
   url "https://github.com/voxibanez/asciiquarium_macos/releases/download/v#{version}/AsciiFishtank-v#{version}.zip",
       verified: "github.com/voxibanez/asciiquarium_macos/"
