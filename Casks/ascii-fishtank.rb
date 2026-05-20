c8f8ac07d571d4a356f6bc75bd1dd04a374f25ff
Tim Dombrowski <voxibanez@gmail.com>
2026-05-19 20:14:32 -0400

    Pin cask checksum for v1.0.9

--- Casks/ascii-fishtank.rb
+++ Casks/ascii-fishtank.rb
@@ -1,6 +1,6 @@
 cask "ascii-fishtank" do
   version "1.0.9"
-  sha256 :no_check
+  sha256 "80f0d2e2acb56e292b7762686d114e68bb95b5df6f07c92c047ad3500b71cfd9"
 
   url "https://github.com/voxibanez/asciiquarium_macos/releases/download/v#{version}/AsciiFishtank-v#{version}.zip",
       verified: "github.com/voxibanez/asciiquarium_macos/"
